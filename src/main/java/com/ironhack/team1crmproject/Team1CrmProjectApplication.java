6380a0fb62d8a620cf92c76597b350b33b0e465c
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-11-21 19:38:13 +0100

    initializing

---
+++ src/main/java/com/ironhack/team1crmproject/Team1CrmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.team1crmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1CrmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1CrmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/team1crmproject/Team1CrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.team1crmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1CrmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
