aa24f08fa1d1e65e844dccf4f12944b1a1a71808
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-11-30 20:17:09 +0100

    Advancing with models

--- src/main/java/com/ironhack/team1crmproject/model/Account.java
+++ src/main/java/com/ironhack/team1crmproject/model/Account.java
@@ -1,13 +1,10 @@
 package com.ironhack.team1crmproject.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.ToString;
 
 
 @Data
@@ -19,30 +16,43 @@ public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "account_id")
     private Long accountId;
 
+    @Column(name = "number_of_employees")
     private int numberOfEmployees;
 
+    @Column(name = "company_name")
     private String companyName;
 
+    @Column(name = "country")
     private String country;
 
+    @Column(name = "city")
     private String city;
 
+    @Column(name = "industry_type")
     private IndustryType industryType;
 
-//    TODO
 
+//    @ToString.Exclude
     @OneToMany(mappedBy = "account")
-    @ToString.Exclude
     private List<Contact> contactList;
 
-//
+//    @ToString.Exclude
     @OneToMany(mappedBy = "account")
-    @ToString.Exclude
     private List<Opportunity> opportunityList;
 
 
+
+    public Account(int numberOfEmployees, String companyName, String country, String city, IndustryType industryType, List<Contact> contactList, List<Opportunity> opportunityList) {
+        this.numberOfEmployees = numberOfEmployees;
+        this.companyName = companyName;
+        this.country = country;
+        this.city = city;
+        this.industryType = industryType;
+        this.contactList = contactList;
+        this.opportunityList = opportunityList;
+    }
 }
 
--- src/main/java/com/ironhack/team1crmproject/model/Lead.java
+++ src/main/java/com/ironhack/team1crmproject/model/Lead.java
@@ -1,4 +1,5 @@
 package com.ironhack.team1crmproject.model;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,17 +15,29 @@ public class Lead {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lead_id")
     private Long leadId;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "role")
     private String role;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "phone_number")
     private String phoneNumber;
 
+    @Column(name = "company_name")
     private String companyName;
 
-
+    public Lead(String name, String role, String email, String phoneNumber, String companyName) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
+    }
 }
