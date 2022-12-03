a0efa762ed7e9e782dc66eabcaf65dbc0f2fe5c4
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-12-03 18:07:21 +0100

    Tests done and Repository methods added

--- src/main/java/com/ironhack/team1crmproject/repository/LeadRepository.java
+++ src/main/java/com/ironhack/team1crmproject/repository/LeadRepository.java
@@ -4,8 +4,17 @@ import com.ironhack.team1crmproject.model.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface LeadRepository extends JpaRepository<Lead,Long> {
+
+    List<Lead> findLeadByLeadId(Long leadId);
+    List<Lead> findLeadByName(String name);
+    List<Lead> findLeadByRole(String role);
+    List<Lead> findLeadByEmail(String email);
+    List<Lead> findLeadByPhoneNumber(String phoneNumber);
+    List<Lead> findLeadByCompanyName(String companyName);
 
 }
--- src/test/java/com/ironhack/team1crmproject/repository/AccountRepositoryTest.java
+++ src/test/java/com/ironhack/team1crmproject/repository/AccountRepositoryTest.java
@@ -1,17 +1,37 @@
 package com.ironhack.team1crmproject.repository;
 
+import com.ironhack.team1crmproject.model.Account;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @BeforeEach
     void setUp() {
+        var account1 = new Account();
+       accountRepository.save(account1);
+    }
+
+    @Test
+    void testingAddingOneAccount() {
+        var countBefore = accountRepository.count();
+        var account2 = new Account();
+        accountRepository.save(account2);
+        var countAfter = accountRepository.count();
+        assertEquals(1, countAfter - countBefore);
     }
 
     @AfterEach
     void tearDown() {
+        accountRepository.deleteAll();
     }
 }
