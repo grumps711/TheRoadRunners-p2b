ac6eaa42b22ec0c3e13b24083fb8e39482d62042
ainga-ri <alfred.inga.rios@gmail.com>
2022-12-06 13:27:49 +0100

    Finished logic, going to test setUp and opportunityTest

--- src/main/java/com/ironhack/team1crmproject/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/team1crmproject/repository/OpportunityRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long>{
+    Opportunity findOpportunityByOpportunityId(Long id);
 }
--- src/test/java/com/ironhack/team1crmproject/repository/OpportunityRepositoryTest.java
+++ src/test/java/com/ironhack/team1crmproject/repository/OpportunityRepositoryTest.java
@@ -1,9 +1,7 @@
 package com.ironhack.team1crmproject.repository;
 
 import com.ironhack.team1crmproject.model.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,46 +9,62 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpportunityRepositoryTest {
-
     OpportunityRepository opportunityRepository;
     ContactRepository contactRepository;
     LeadRepository leadRepository;
-
     AccountRepository accountRepository;
     @BeforeEach
     void setUp() {
         // Creation of Leads
         var basicLeads = List.of(
                 new Lead("Eugeni", "Ironhacker", "eugeni@gmail.com", "666666666", "Ironhack"),
-                new Lead("Jose", "Ironhacker", "jose@gmail.com", "666666666", "Ironhack")
+                new Lead("Jose", "Ironhacker", "jose@gmail.com", "666666666", "Ironhack"),
+                new Lead("Silvi", "Catalonier", "silvi@gmail.com", "999999999", "CataloniaHotels")
         );
         leadRepository.saveAll(basicLeads);
         var eugeniLead = basicLeads.get(0);
         var joseLead = basicLeads.get(1);
+        var silviLead = basicLeads.get(2);
 
-        // Creation of Contacts
-        var contacts = List.of(
-                new Contact(eugeniLead.getName(), eugeniLead.getRole(), eugeniLead.getEmail(), eugeniLead.getPhoneNumber()),
-                new Contact(joseLead.getName(), joseLead.getRole(), joseLead.getEmail(), joseLead.getPhoneNumber())
-        );
-        contactRepository.saveAll(contacts);
-
-        // Creation of Contacts
-        var accounts = List.of(
-                new Account(2, eugeniLead.getCompanyName(), "England", "London", IndustryType.MANUFACTURING),
-                new Account(4, joseLead.getCompanyName(), "Switzerland", "Zurich", IndustryType.ECOMMERCE)
-        );
-        accountRepository.saveAll(accounts);
-        //Creation of Opportunities
+        // Transform Lead to Opportunity
         var opportunities = List.of(
-                new Opportunity(TruckType.BOX, 6, contacts.get(0)),
-                new Opportunity(TruckType.HYBRID, 2, contacts.get(1))
+                createANewOpportunity(eugeniLead, TruckType.BOX, 6),
+                createANewOpportunity(joseLead, TruckType.FLATBED, 13),
+                createANewOpportunity(silviLead, TruckType.HYBRID, 30)
         );
         opportunityRepository.saveAll(opportunities);
+
+        // Creation of Accounts just after created an opportunity
+        var accounts = List.of(
+                getAccount(eugeniLead.getCompanyName()),
+                getAccount(silviLead.getCompanyName())
+        );
+        accountRepository.saveAll(accounts);
     }
     @AfterEach
     void tearDown() {
         opportunityRepository.deleteAll();
+    }
+
+    @Nested
+    @DisplayName("Check SetUp")
+    class checkSetUp {
+        @Test
+        void checkTotalLeadsAfterCreateOpportunity() {
+            assertEquals(1, leadRepository.findAll().size());
+        }
+        @Test
+        void checkTotalOpportunities() {
+            assertEquals(1, opportunityRepository.findAll().size());
+        }
+        @Test
+        void checkTotalContacts() {
+            assertEquals(1, contactRepository.findAll().size());
+        }
+        @Test
+        void checkTotalAccounts() {
+            assertEquals(1, accountRepository.findAll().size());
+        }
     }
 
     @Test
@@ -62,22 +76,23 @@ class OpportunityRepositoryTest {
         // convert 134
         var alfredOpportunity = createANewOpportunity(alfredLead, TruckType.BOX, 6);
         alfredOpportunity = opportunityRepository.save(alfredOpportunity);
-        // TODO: TEST checks alfredLead is deleted
+        assertNull(leadRepository.findLeadByLeadId(alfredLead.getLeadId()));
+        assertEquals(alfredOpportunity, opportunityRepository.findOpportunityByOpportunityId(alfredOpportunity.getOpportunityId()));
+
         // after opportunity creation, account must be created or updated
         Account account = getAccount(alfredLead.getCompanyName());
-
+        assertEquals(account, accountRepository.findAccountByAccountId(account.getAccountId()));
         // add opportunity and contact to the Account
         account.getOpportunityList().add(alfredOpportunity);
         account.getContactList().add(getContact(alfredLead));
-
     }
     private Opportunity createANewOpportunity(Lead lead, TruckType truck, int quantity) {
         Opportunity opportunity;
         Contact decisionMaker = getContact(lead);
         opportunity = new Opportunity(truck, quantity, decisionMaker);
+        leadRepository.delete(leadRepository.findLeadByLeadId(lead.getLeadId()));
         return opportunity;
     }
-
     private Account getAccount(String companyName) {
         var accounts = accountRepository.findAll();
         int i = 0;
@@ -91,6 +106,7 @@ class OpportunityRepositoryTest {
             i++;
         }
         if (!found) {
+            // input from user
             account = new Account(2, companyName, "England", "London", IndustryType.MANUFACTURING);
             accountRepository.save(account);
         }
@@ -114,4 +130,6 @@ class OpportunityRepositoryTest {
         }
         return decisionMaker;
     }
+    @Test
+    void
 }
